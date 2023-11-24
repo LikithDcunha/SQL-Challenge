6167214ab35648dfa9e4f5792d9e6af57bf8d489
Likith Dcunha <likithdc@gmail.com>
2023-11-24 23:07:14 +0100

    Query result

--- 1_Olympics_Data/Queries.sql
+++ 1_Olympics_Data/Queries.sql
@@ -1,5 +1,22 @@
 --1)  What are the number of games played in all summer olympics.
 
-select * 
-from games_competitor 
-limit 10;
+-- using CTE 
+
+WITH t1 as (   
+    SELECT  s.sport_name, COUNT(DISTINCT(g.games_name)) as num_games_where_played
+    FROM games g
+    inner Join games_competitor gc on g.id = gc.games_id
+    inner Join competitor_event ce on gc.id = ce.competitor_id
+    inner join event ev on ce.event_id = ev.id
+    inner Join sport s on ev.sport_id = s.id 
+    where g.season = 'Summer'
+    GROUP BY 1 
+    ORDER BY 2 DESC ) , 
+t2 as 
+    (select COUNT(g.games_name) as total_summer_games
+    from games g 
+    where season = 'Summer')
+
+SELECT * 
+from t1 
+Join t2 on t2.total_summer_games = t1.num_games_where_played ;
